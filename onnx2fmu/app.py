a7034e0fe751be8bf3e5c541893e6a2a941f6e0d
Michele Urbani <murbani@fbk.eu>
2024-12-23 16:46:07 +0100

    Added python app and templates.

--- onnx2fmu/app.py
+++ onnx2fmu/app.py
@@ -18,8 +18,8 @@ template_path = Path(TEMPLATE_DIR)
 
 # C types are strings because they are simply copied in the template
 onnx_to_c_types = {
-    "FLOAT": "float",  # This is the most common
-    "DOUBLE": "double",
+    "FLOAT": {"FMI2": "Real", "FMI3": "Float32", "C": "float"},  # This is the most common
+    "DOUBLE": {"FMI2": "Real", "FMI3": "Float64", "C": "double"},
 }
 
 
@@ -64,7 +64,7 @@ def cleanName(name: str) -> str:
     return name.replace("/", "_").replace(":", "")
 
 
-def generateIOEntries(model, context):
+def FMI2generateIOEntries(model):
     """
     Parse the ONNX model and generate input/output entries for the FMU.
 
@@ -73,16 +73,19 @@ def generateIOEntries(model, context):
     - ``model`` (onnx.ModelProto): The ONNX model.
     - ``context`` (dict): The context dictionary for templating.
     """
+    context = {}
+    # Add all input nodes as multi-dimensional variables
+    # Node names must be cleaned from C-unsupported characters
     input_name = cleanName(model.graph.input[0].name)
-    # Print the input node name
-    logger.debug(f"Input node: {input_name}")
     # Retrieve input tensor shape
     shape = tuple(dim.dim_value for dim in
-                  model.graph.input[0].type.tensor_type.shape.dim)
-    # Print input tensor shape as a list of integers
-    logger.debug(f"Input shape: {shape}")
+                    model.graph.input[0].type.tensor_type.shape.dim)
+    # Print the input node name
+    logger.debug(f"Input node {input_name} with shape {shape}.")
     # A unique index is used for all model variables
     i = 0
+    # FMI2 does not support multi-dimensional inputs, so we need to flatten
+    # the input tensor.
     # Initialize the input entries list
     context['input_entries'] = []
     # Iterate over all dimensions except the first one
@@ -101,12 +104,10 @@ def generateIOEntries(model, context):
         i += 1
     # Print the output node name
     output_name = model.graph.output[0].name
-    logger.debug(f"Output node: {output_name}")
     # Retrieve output tensor shape
     shape = tuple(dim.dim_value for dim in
-                  model.graph.output[0].type.tensor_type.shape.dim)
-    # Print input tensor shape as a list of integers
-    logger.debug(f"Output shape: {shape}")
+                model.graph.output[0].type.tensor_type.shape.dim)
+    logger.debug(f"Output node: {output_name} with shape {shape}.")
     # Initialize the output entries list
     context['output_entries'] = []
     # Iterate over all dimensions except the first one
@@ -120,6 +121,68 @@ def generateIOEntries(model, context):
             'variability': "continuous",
         })
         i += 1
+    return context
+
+
+def FMI3generateIOEntries(model):
+    """
+    Parse the ONNX model and generate input/output entries for the FMU.
+
+    Parameters:
+    -----------
+    - ``model`` (onnx.ModelProto): The ONNX model.
+    - ``context`` (dict): The context dictionary for templating.
+    """
+    # A unique index is used for all model variables
+    i = 0
+    context = {}
+    # Initialize the input entries list
+    context['input_entries'] = []
+    # Add all input nodes as multi-dimensional variables
+    for node in model.graph.input:
+        # Node names must be cleaned from C-unsupported characters
+        input_name = cleanName(model.graph.input[0].name)
+        # Retrieve input tensor shape
+        shape = tuple(dim.dim_value for dim in
+                      model.graph.input[0].type.tensor_type.shape.dim)
+        # Print the input node name
+        logger.debug(f"Input node {input_name} with shape {shape}.")
+        # FMI3 supports multi-dimensional variables.
+        # Iterate over all input nodes
+        element_type = TensorProto.DataType.Name(
+            node.type.tensor_type.elem_type
+        )
+        context['input_entries'].append({
+            'name': f"{input_name}",
+            'index': i,
+            'type': onnx_to_c_types[element_type],
+            'description': "Input name identifies tensor element.",
+            'causality': "independent",
+            'variability': "continuous",
+            'dimensions': shape,
+        })
+        i += 1
+        # Print the output node name
+    for node in model.graph.output:
+        output_name = model.graph.output[0].name
+        # Retrieve output tensor shape
+        shape = tuple(dim.dim_value for dim in
+                      model.graph.output[0].type.tensor_type.shape.dim)
+        logger.debug(f"Output node: {output_name} with shape {shape}.")
+        # Initialize the output entries list
+        context['output_entries'] = []
+        # Iterate over all dimensions except the first one
+        context['output_entries'].append({
+            'name': f"out{output_name}",
+            'index': i,
+            'type': onnx_to_c_types[element_type],
+            'description': "Output name identifies tensor element.",
+            'causality': "output",
+            'variability': "continuous",
+            'dimensions': shape,
+        })
+        i += 1
+    return context
 
 
 @app.command()
@@ -162,7 +225,8 @@ def build(
     # Read the model file
     model = load(model_path)
     # Generate input entries for all dimensions except the first one
-    generateIOEntries(model, context)
+    fmi2ModelVariables = FMI2generateIOEntries(model)
+    fmi3ModelVariables = FMI3generateIOEntries(model)
 
     ############################
     # Populate the templates
@@ -177,18 +241,23 @@ def build(
     # Create a Jinja2 environment and set the current directory as the search
     # path
     env = Environment(loader=FileSystemLoader("."))
-    # Iterate over all templates
+    # Iterate over all the remaining templates
     for template_name in template_path.iterdir():
-        # Skip directories
+        # Skip directories and FMI files
         if not template_name.is_file():
             continue
+        # Catch FMI2 and FMI3 templates
+        if "FMI2" in template_name.name:
+            context.update(fmi2ModelVariables)
+        elif "FMI3" in template_name.name:
+            context.update(fmi3ModelVariables)
         # Load the template
         template = env.get_template(str(template_name))
         # Render the template with the context
-        output = template.render(context)
-        # Write the output to a file
+        out = template.render(context)
+        # Write the rendered template to the target directory
         with open(target_path / f"{template_name.name}", "w") as f:
-            f.write(output)
+            f.write(out)
 
     # Copy the model to the resources directory, do not change
     model_target_path = target_path / "resources/model.onnx"
--- template/config.h
+++ template/config.h
@@ -2,23 +2,33 @@
 #define config_h
 
 // define class name and unique id
-#define MODEL_IDENTIFIER Resource
-#define INSTANTIATION_TOKEN "{7b9c2114-2ce5-4076-a138-2cbc69e069e5}"
+#define MODEL_IDENTIFIER {{ model_name }}
+#define INSTANTIATION_TOKEN "{{ model_guid }}"
 
 #define CO_SIMULATION
-#define MODEL_EXCHANGE
 
-#define GET_INT32
+#define GET_FLOAT32
+#define SET_FLOAT32
 
 #define FIXED_SOLVER_STEP 1
 #define DEFAULT_STOP_TIME 1
 
 typedef enum {
-    vr_time, vr_y
+    {%- for input in input_entries %}
+    vr_{{ input.name }},
+    {%- endfor %}
+    {%- for output in output_entries %}
+    vr_{{ output.name }},
+    {%- endfor %}
 } ValueReference;
 
 typedef struct {
-    int y;
+    {%- for input in input_entries %}
+    float {{ input.name }};
+    {%- endfor %}
+    {%- for output in output_entries %}
+    float {{ output.name }};
+    {%- endfor %}
 } ModelData;
 
 #endif /* config_h */

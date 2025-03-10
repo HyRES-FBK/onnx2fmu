3d29dfc2c403fd3216fc55894268678440d1b802
Michele Urbani <murbani@fbk.eu>
2025-03-10 11:50:45 +0100

    Added setFloat64 to model.c.

--- template/model.c
+++ template/model.c
@@ -261,7 +261,43 @@ Status getFloat64(ModelInstance *comp, ValueReference vr, double values[],
         logError(comp, "getFloat64: ValueReference %d not available.", vr);
         return Error;
     }
+}
 
+Status setFloat64(ModelInstance *comp, ValueReference vr, const double values[],
+                  size_t nValues, size_t *index) {
+    // Switch on the value reference
+    switch (vr)
+    {
+        // Time is always a double value
+        case vr_time:
+            ASSERT_NVALUES(1);
+            M(time) = values[(*index)++];
+            return OK;
+#if FMI_VERSION < 3
+        // Inputs
+        {%- for input in inputs %}
+        {%- for scalar in input.scalarValues %}
+        case vr_{{ cleanName(scalar.name) }}:
+            ASSERT_NVALUES(1);
+            M({{ cleanName(scalar.name) }}) = values[(*index)++];
+            return OK;
+        {%- endfor %}
+        {%- endfor %}
+        // Outputs
+        {%- for output in outputs %}
+        {%- for scalar in output.scalarValues %}
+        case vr_{{ cleanName(scalar.name) }}:
+            ASSERT_NVALUES(1);
+            M({{ cleanName(scalar.name) }}) = values[(*index)++];
+            return OK;
+        {%- endfor %}
+        {%- endfor %}
+#endif
+    default:
+        // Compose message for log with value reference
+        logError(comp, "setFloat64: ValueReference %d not available.", vr);
+        return Error;
+    }
 }
 
 Status eventUpdate(ModelInstance *comp) {

d3b0f7685cfc6d8be258d39984628971aa762770
Michele Urbani <murbani@fbk.eu>
2025-03-10 11:38:40 +0100

    Completed calculateValues in model.c.

--- template/model.c
+++ template/model.c
@@ -55,12 +55,12 @@ Status calculateValues(ModelInstance *comp) {
             OrtArenaAllocator, OrtMemTypeDefault, &memory_info
         ),
         comp);
-
-    // Create input tensors
-    {%- for input in input_entries %}
+    {% for input in inputs %}
+    // Create {{ cleanName(input.name) }} tensor
     OrtValue* {{ cleanName(input.name) }}_tensor;
 
-    const int64_t {{ cleanName(input.name) }}_shape[] = { {{ input.dimensions|join(", ") }} };
+    // Store the shape of the input tensor
+    const int64_t {{ cleanName(input.name) }}_shape[] = { {{ input.shape|join(", ") }} };
 
     // Determine the dimensions of the input tensor
     const int64_t {{ cleanName(input.name) }}_dim = sizeof({{ cleanName(input.name) }}_shape) / sizeof({{ cleanName(input.name) }}_shape[0]);
@@ -69,170 +69,160 @@ Status calculateValues(ModelInstance *comp) {
         {{ cleanName(input.name) }}_size *= {{ cleanName(input.name) }}_shape[i];
     }
 
+    // Store values in the flattened array
+    float* {{ cleanName(input.name) }}_float = (float*)malloc({{ cleanName(input.name) }}_size * sizeof(float));
+    if ({{ cleanName(input.name) }}_float == NULL) {
+        logError(comp, "Failed to allocate memory for {{ cleanName(input.name) }}_float");
+        return Error;
+    }
+
     // Flatten the input array
-    float* input_float = (float*)malloc({{ cleanName(input.name) }}_size * sizeof(float));
-    int64_t index = 0;
-    flatten_array(
-        (double*)M({{ cleanName(input.name) }}), input_float, &index,
-        {{ cleanName(input.name) }}_shape, {{ cleanName(input.name) }}_dim, 0
-    );
+    {%- for scalar in input.scalarValues %}
+    {{ cleanName(input.name) }}_float[{{ loop.index0 }}] = (float)M({{ cleanName(scalar.name) }});
+    {%- endfor %}
 
     ORT_ABORT_ON_ERROR(
         comp->g_ort->CreateTensorWithDataAsOrtValue(
             memory_info,
-            input_float, {{ cleanName(input.name) }}_size * sizeof(float),
-            {{ cleanName(input.name) }}_shape, {{ cleanName(input.name) }}_dim,
+            {{ cleanName(input.name) }}_float,
+            {{ cleanName(input.name) }}_size * sizeof(float),
+            {{ cleanName(input.name) }}_shape,
+            {{ cleanName(input.name) }}_dim,
             ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT,
             &{{ cleanName(input.name) }}_tensor
         ),
         comp);
 
-    int is_tensor;
+    int {{ cleanName(input.name) }}_is_tensor;
     ORT_ABORT_ON_ERROR(
-        comp->g_ort->IsTensor({{ cleanName(input.name) }}_tensor, &is_tensor),
-        comp);
+        comp->g_ort->IsTensor(
+            {{ cleanName(input.name) }}_tensor,
+            &{{ cleanName(input.name) }}_is_tensor),
+            comp
+        );
 
-    assert(is_tensor);
-
-    {%- endfor %}
+    assert({{ cleanName(input.name) }}_is_tensor);
+    {% endfor -%}
 
     // Release the memory info
     comp->g_ort->ReleaseMemoryInfo(memory_info);
 
+    // Create output tensors
+    {%- for output in outputs %}
+    OrtValue* {{ cleanName(output.name) }}_tensor = NULL;
+    {%- endfor %}
+
+    // Declare input node names
+    const char* input_names[] = {
+        {%- for input in inputs %}
+        "{{ cleanName(input.name) }}"{% if not loop.last %},{% endif %}
+        {%- endfor %}
+    };
+
+    // Declare output node names
+    const char* output_names[] = {
+        {%- for output in outputs %}
+        "{{ cleanName(output.name) }}"{% if not loop.last %},{% endif %}
+        {%- endfor %}
+    };
+
+    // Gather input tensors
+    const OrtValue* input_tensors[] = {
+        {%- for input in inputs %}
+        {{ cleanName(input.name) }}_tensor{% if not loop.last %},{% endif %}
+        {%- endfor %}
+    };
+
+    // Gather output tensors
+    OrtValue* output_tensors[] = {
+        {%- for output in outputs %}
+        {{ cleanName(output.name) }}_tensor{% if not loop.last %},{% endif %}
+        {%- endfor %}
+    };
+
     // Run inference
-    OrtValue* output_tensor = NULL;
-    const char* input_names[] = { "{{ input_entries[0].name }}" };
-    const char* output_names[] = { "{{ output_entries[0].name }}" };
     ORT_ABORT_ON_ERROR(
         comp->g_ort->Run(
-            comp->session, NULL, input_names,
-            (const OrtValue* const*) &{{ cleanName(input_entries[0].name) }}_tensor,
-            1, output_names, 1, &output_tensor
+            comp->session,
+            NULL,
+            input_names,
+            input_tensors,
+            {{ inputs|length }},
+            output_names,
+            {{ outputs|length }},
+            output_tensors
         ),
         comp
     );
 
+    // Check output tensors to be tensors
+    {%- for output in outputs %}
+    int {{ cleanName(output.name) }}_is_tensor;
     ORT_ABORT_ON_ERROR(
-        comp->g_ort->IsTensor(output_tensor, &is_tensor),
-        comp);
-
-    assert(is_tensor);
-
-    // Retrieve a pointer to output tensor data
-    float* output_tensor_data = NULL;
-    ORT_ABORT_ON_ERROR(
-        comp->g_ort->GetTensorMutableData(output_tensor, (void**)&output_tensor_data),
-        comp);
-
-    OrtTensorTypeAndShapeInfo* tensor_info;
-    ORT_ABORT_ON_ERROR(
-        comp->g_ort->GetTensorTypeAndShape(output_tensor, &tensor_info),
-        comp);
-    // Output tensor dimension
-    size_t output_tensor_dim;
-    ORT_ABORT_ON_ERROR(
-        comp->g_ort->GetTensorShapeElementCount(tensor_info, &output_tensor_dim),
-        comp);
-
-    {%- for output in output_entries %}
-    for (size_t i = 0; i < output_tensor_dim; i++) {
-        float f = output_tensor_data[i];
-        M({{ output_entries[0].name }})[{{ loop.index0 }}][i] = f;
-    }
+        comp->g_ort->IsTensor(
+            {{ cleanName(output.name) }}_tensor,
+            &{{ cleanName(output.name) }}_is_tensor),
+            comp
+        );
+    assert ({{ cleanName(output.name) }}_is_tensor);
     {%- endfor %}
 
-    // Release the input tensor
-    {%- for input in input_entries %}
-    comp->g_ort->ReleaseValue({{ cleanName(input.name) }}_tensor);
+    {%- for output in outputs %}
+    // Retrieve pointer to the {{ cleanName(output.name )}} tensor
+    float* {{ cleanName(output.name) }}_tensor_data = NULL;
+    ORT_ABORT_ON_ERROR(
+        comp->g_ort->GetTensorMutableData(
+            output_tensors[{{ loop.index0 }}],
+            (void**)&{{ cleanName(output.name) }}_tensor_data
+        ),
+        comp
+    );
+
+    // Retrieve {{ cleanName(output.name )}} tensor info
+    OrtTensorTypeAndShapeInfo* {{ cleanName(output.name) }}_info;
+    ORT_ABORT_ON_ERROR(
+        comp->g_ort->GetTensorTypeAndShape(
+            output_tensors[{{ loop.index0 }}],
+            &{{ cleanName(output.name) }}_info
+        ),
+        comp
+    );
+
+    // Retrieve {{ cleanName(output.name )}} tensor shape
+    size_t {{ cleanName(output.name) }}_dims;
+    ORT_ABORT_ON_ERROR(
+        comp->g_ort->GetDimensionsCount(
+            {{ cleanName(output.name) }}_info,
+            &{{ cleanName(output.name) }}_dims
+        ),
+        comp
+    );
+
+    // Set {{ cleanName(output.name )}} tensor data to model
+    {%- for scalar in output.scalarValues %}
+    M({{ cleanName(scalar.name) }}) = {{ cleanName(output.name) }}_tensor_data[{{ loop.index0 }}];
     {%- endfor %}
-    // Release the output tensor
-    {%- for output in output_entries %}
+
+    {%- endfor %}
+
+    // Free tensors
+    {%- for output in outputs %}
     comp->g_ort->ReleaseValue({{ cleanName(output.name) }}_tensor);
     {%- endfor %}
+    {%- for input in inputs %}
+    comp->g_ort->ReleaseValue({{ cleanName(input.name) }}_tensor);
+    {%- endfor %}
 
-    free(input_float);
+    // Free arrays
+    {%- for output in outputs %}
+    free({{ cleanName(output.name) }}_tensor_data);
+    {%- endfor %}
+    {%- for input in inputs %}
+    free({{ cleanName(input.name) }}_float);
+    {%- endfor %}
 
     return OK;
 }
-
-
-Status getFloat64(ModelInstance *comp, ValueReference vr, double values[],
-                  size_t nValues, size_t *index) {
-
-    // Calculate values
-    calculateValues(comp);
-
-    switch (vr)
-    {
-        // Time is always a double value
-        case vr_time:
-            ASSERT_NVALUES(1);
-            values[(*index)++] = M(time);
-            return OK;
-#if FMI_VERSION < 3
-        // Inputs
-        {%- for input in input_entries %}
-        {%- for i, idx in input.indexes.items() %}
-        case vr_{{ cleanName(input.name) }}_{{ idx|join('_') }}:
-            ASSERT_NVALUES(1);
-            values[(*index)++] = M({{ cleanName(input.name) }}){% for j in idx %}[{{ j }}]{% endfor %};
-            return OK;
-        {%- endfor %}
-        {%- endfor %}
-        // Outputs
-        {%- for output in output_entries %}
-        {%- for i, idx in output.indexes.items() %}
-        case vr_{{ cleanName(output.name) }}_{{ idx|join('_') }}:
-            ASSERT_NVALUES(1);
-            values[(*index)++] = M({{ cleanName(output.name) }}){% for j in idx %}[{{ j }}]{% endfor %};
-            return OK;
-        {%- endfor %}
-        {%- endfor %}
-#endif
-        default:
-            // Compose message for log with value reference
-            logError(comp, "GetFloat64: ValueReference %d not available.", vr);
-            return Error;
-    }
-}
-
-
-Status setFloat64(ModelInstance *comp, ValueReference vr, const double values[],
-                  size_t nValues, size_t *index) {
-    // Switch for value references
-    switch (vr)
-    {
-        // Time is always a double value
-        case vr_time:
-            ASSERT_NVALUES(1);
-            M(time) = values[(*index)++];
-            return OK;
-#if FMI_VERSION < 3
-        // Inputs
-        {%- for input in input_entries %}
-        {%- for i, idx in input.indexes.items() %}
-        case vr_{{ cleanName(input.name) }}_{{ idx|join('_') }}:
-            ASSERT_NVALUES(1);
-            M({{ cleanName(input.name) }}){% for j in idx %}[{{ j }}]{% endfor %} = values[(*index)++];
-            return OK;
-        {%- endfor %}
-        {%- endfor %}
-        // Outputs
-        // {%- for output in output_entries %}
-        // {%- for i, idx in output.indexes.items() %}
-        // case vr_{{ cleanName(output.name) }}_{{ idx|join('_') }}:
-        //     ASSERT_NVALUES(1);
-        //     M({{ cleanName(output.name) }}){% for j in idx %}[{{ j }}]{% endfor %} = values[(*index)++];
-        //     return OK;
-        // {%- endfor %}
-        // {%- endfor %}
-#endif
-    default:
-        logError(comp, "SetFloat64: ValueReference %d not available.", vr);
-        return Error;
-    }
-}
-
 
 Status eventUpdate(ModelInstance *comp) {
 

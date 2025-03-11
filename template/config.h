ddf9b163eef576ddcbbb1937d463d8d2f64b338e
Michele Urbani <murbani@fbk.eu>
2025-03-11 17:03:42 +0100

    Change config.h with new variable names and recipe for FMI2 only.

--- template/config.h
+++ template/config.h
@@ -2,8 +2,8 @@
 #define config_h
 
 // define class name and unique id
-#define MODEL_IDENTIFIER {{ model_name }}
-#define INSTANTIATION_TOKEN "{{ model_GUID }}"
+#define MODEL_IDENTIFIER {{ name }}
+#define INSTANTIATION_TOKEN "{{ GUID }}"
 
 #define CO_SIMULATION
 
@@ -21,22 +21,15 @@ typedef enum {
     // Always include time
     vr_time,
 #if FMI_VERSION < 3
-    {%- for input in input_entries %}
-    {%- for i, idx in input.indexes.items() %}
-    vr_{{ cleanName(input.name) }}_{{ idx|join('_') }},
+    {%- for input in inputs %}
+    {%- for scalar in input.scalarValues %}
+    vr_{{ cleanName(scalar.name) }},
     {%- endfor %}
     {%- endfor %}
-    {%- for output in output_entries %}
-    {%- for i, idx in output.indexes.items() %}
-    vr_{{ cleanName(output.name) }}_{{ idx|join('_') }},
+    {%- for output in outputs %}
+    {%- for scalar in output.scalarValues %}
+    vr_{{ cleanName(scalar.name) }},
     {%- endfor %}
-    {%- endfor %}
-#else
-    {%- for input in input_entries %}
-    vr_{{ cleanName(input.name) }},
-    {%- endfor %}
-    {%- for output in output_entries %}
-    vr_{{ cleanName(output.name) }},
     {%- endfor %}
 #endif
 } ValueReference;
@@ -44,12 +37,18 @@ typedef enum {
 typedef struct {
     // Always include time
     double time;
-    {%- for input in input_entries %}
-    double {{ cleanName(input.name) }}{% for dim in input.dimensions %}[{{ dim }}]{% endfor %};
+#if FMI_VERSION < 3
+    {%- for input in inputs %}
+    {%- for scalar in input.scalarValues %}
+    double {{ cleanName(scalar.name) }};
     {%- endfor %}
-    {%- for output in output_entries %}
-    double {{ cleanName(output.name) }}{% for dim in output.dimensions %}[{{ dim }}]{% endfor %};
     {%- endfor %}
+    {%- for output in outputs %}
+    {%- for scalar in output.scalarValues %}
+    double {{ cleanName(scalar.name) }};
+    {%- endfor %}
+    {%- endfor %}
+#endif
 } ModelData;
 
 #endif /* config_h */
